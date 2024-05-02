37647591a800ad9b565a596bc71d36596decbd90
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-02 17:16:21 +0200

    feat: all-links

--- src/components/AppFooterBottom.vue
+++ src/components/AppFooterBottom.vue
@@ -39,25 +39,36 @@
 <template>
     <footer>
             <div class="footer-bottom small-wrapper" >
-                Footer Left
+                <div class="all-links">
+                    <ul v-for="(curElem, index) in allLinks">
+                        <span>{{ curElem.name }}</span>
+                        <li v-for="elem in allLinks[index].links"><a href="">{{ elem }}</a></li>
+                    </ul>
+                </div>
                 <img :src="getImagePath(dcLogo)" alt="">
             </div>
     </footer>
 </template>
 
 <style scoped lang="scss">
+    @use "../style/partials/variables" as *;
+
     footer{
         background-image: url(/src/assets/img/footer-bg.jpg);
         background-repeat: no-repeat;
         overflow: hidden;
 
         .footer-bottom{
+            padding: 30px 0;
             margin: 0 auto;
             color: white;
             display: flex;
             justify-content: space-between;
             position: relative;
             height: 300px;
+            ul {
+                list-style: none;
+            }
         }
     }
 
@@ -67,5 +78,23 @@
         top: -30%;
         right: 0;
     }
+
+    .all-links {
+        display: flex;
+        gap: 10px;
+    }
+
+    a{
+        text-decoration: none;
+        color: $main-grey;
+        font-size: 0.6rem;
+    }
+
+    span {
+        text-transform: uppercase;
+        font-weight: bold;
+    }
+
+    
 
 </style>
